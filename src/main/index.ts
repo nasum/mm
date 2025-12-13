15dd7156aa6bd6f24538e5d205a89159d933c617
nasum <tomato.wonder.life@gmail.com>
2025-12-14 02:05:05 +0900

    edit icon

--- src/main/index.ts
+++ src/main/index.ts
@@ -73,7 +73,7 @@ function createWindow(): void {
     y: bounds?.y,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    icon,
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false,
